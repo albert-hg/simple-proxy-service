756373b183af84b9db42b06e1f65286b88774530
albert.huang <albert.huang@neutec.com.tw>
2022-02-08 10:37:45 +0800

    init

---
+++ src/main/java/com/albert/simpleproxyservice/SimpleProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.albert.simpleproxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProxyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albert/simpleproxyservice/SimpleProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.simpleproxyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
